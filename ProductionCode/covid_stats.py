d0ba78c1d28450b1b93f2cd6079bb2c98deee7a9
Fenan Gudina <fenan@Fenans-MacBook-Pro.local>
2025-04-21 11:34:27 -0500

    stats and highest

--- ProductionCode/covid_stats.py
+++ ProductionCode/covid_stats.py
@@ -1,1 +1,25 @@
+import csv
 
+def stats (country, week):
+    with open ('Data/WHO-COVID-19-global-data.csv', 'r') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            if row['Country'] == country and row['Date_reported'] == week:
+                print(f"Country: {country}")
+                print(f"Week: {week}")
+                print(f"Cases: {row['New_cases']}")
+                print(f"Deaths: {row['New_deaths']}")
+                return            
+
+def highest (week):
+    with open('Data/WHO-COVID-19-global-data.csv', 'r' ) as file:
+        reader = csv.DictReader(file)
+        highest_cases = 0
+        highest_country = ""
+        for row in reader:
+            if row['Date_reported'] == week:
+                if int(row['New_cases']) > highest_cases:
+                    highest_cases = int(row['New_cases'])
+                    highest_country = row['Country']
+        print(f"Country with the highest cases in {week}: {highest_country} with {highest_cases} cases")
