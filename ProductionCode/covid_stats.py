bdabef04a999cf6adb26c1137258886e68ca08f6
Fenan Gudina <fenan@Fenans-MacBook-Pro.local>
2025-04-21 20:15:22 -0500

    final

--- ProductionCode/covid_stats.py
+++ ProductionCode/covid_stats.py
@@ -1,16 +1,24 @@
 import csv
+from datetime import datetime
 
-def stats (country, week):
-    with open ('Data/WHO-COVID-19-global-data.csv', 'r') as file:
+def stats(country, beginning_date, ending_date):
+    beginning = datetime.strptime(beginning_date, "%Y-%m-%d")
+    ending = datetime.strptime(ending_date, "%Y-%m-%d")
+
+    total_cases = 0
+    total_deaths = 0
+
+    with open('Data/WHO-COVID-19-global-data.csv', 'r') as file:
         reader = csv.DictReader(file)
 
         for row in reader:
-            if row['Country'] == country and row['Date_reported'] == week:
-                print(f"Country: {country}")
-                print(f"Week: {week}")
-                print(f"Cases: {row['New_cases']}")
-                print(f"Deaths: {row['New_deaths']}")
-                return            
+            if row['Country'] == country:
+                date = datetime.strptime(row['Date_reported'], "%Y-%m-%d")
+                if beginning <= date <= ending:
+                    total_cases += int(row['New_cases'])
+                    total_deaths += int(row['New_deaths'])
+
+    return total_cases, total_deaths
 
 def highest (week):
     with open('Data/WHO-COVID-19-global-data.csv', 'r' ) as file:
