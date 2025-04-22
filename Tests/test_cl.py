784d78902f089bbb991873560cc0e4da9a46ed18
Daniel Zhang <danielzhang@Daniels-Laptop.local>
2025-04-21 21:12:08 -0500

    First tests commit

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,1 +1,29 @@
+import unittest
+from io import StringIO
+import sys
+import cl
+from ProductionCode import covid_stats
 
+class TestCovidStats(unittest.TestCase):
+    
+    def test_highest(self):
+        self.assertEqual(covid_stats.highest('2020-01-01'), "data_1_1")
+        
+    def test_stats(self):
+        self.assertEqual(covid_stats.stats('Afghanistan', '2020-01-01', '2020-01-12'), (0, 0))
+        
+    def test_compare(self):
+        self.assertEqual(covid_stats.compare(['Afghanistan', 'Albania'], '2020-01-01', '2020-01-12'), (0, 0))
+        
+    def test_cl_main(self):
+        sys.argv = ['cl.py', 'stats', 'Afghanistan', '2020-01-01']
+        i = 3
+        while i != 0:
+            i -= 1
+            sys.stdout = StringIO()
+            cl.main()
+            printed_output = sys.stdout.getvalue()
+            self.assertEqual(printed_output, """Usage:\n  python cl.py compare country1,country2 beginning_date ending_date\n  python cl.py stats country beginning_date ending_date\n  python cl.py highest beginning_date ending_date""")
+            sys.argv.pop()
+    
+    
