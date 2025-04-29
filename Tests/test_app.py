638beaad7223fd0c25ab80ad79481034d14f51b5
Anthony Vazquez - Vazquez <vazqueza@carleton.edu>
2025-04-28 21:03:28 -0500

    test

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import unittest
-from io import StringIO
-import sys
-import app
-
-class TestCovidStats(unittest.TestCase):
-    """Test file for our flask app"""
-    def test_about_page(self):
-        self.assertEqual(app.aboutpage(), "Hello, this is the about page.")
-        
-    def test_cl_main_not_enough_args(self):
-        pass
-=======
 """uniit tests for the Flask application for COVID-19 stats comparison"""
 import unittest
 from app import app
@@ -31,4 +17,3 @@ class TestFlaskApp(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
->>>>>>> 2dca5ef (test)
