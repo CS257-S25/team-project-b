6291fc3da46cbd8355456ed0c365e7cca43e7aca
DanielZhang <danielzhang@Daniels-Laptop.local>
2025-05-06 21:59:11 -0500

    Tests

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -92,7 +92,7 @@ class TestCovidStats(unittest.TestCase):
         cl.main()
         printed_output = sys.stdout.getvalue()
         self.assertEqual(printed_output, """Total cases in Afghanistan from 2020-01-01 to 2020-01-12: 0!\n
-          Total deaths in Afghanistan from 2020-01-01 to {2020-01-12: 0!\n""")
+          Total deaths in Afghanistan from 2020-01-01 to 2020-01-12: 0!\n""")
         
     def test_cl_main_compare(self):
         """Test function for the command line main function when compare is requested"""
