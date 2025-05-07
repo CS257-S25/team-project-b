9fcfc32534d4b169b5c95129830523876b47b6f7
DanielZhang <danielzhang@Daniels-Laptop.local>
2025-05-06 21:36:24 -0500

    Tests

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -73,16 +73,6 @@ Total deaths in Albania from 2020-01-01: 0\n\n""")
         cl.main()
         printed_output = sys.stdout.getvalue()
         self.assertEqual(printed_output, """s\n""")
-    
-    
-    def test_cl_main_stats(self):
-        """Test function for the command line main function when stats are requested"""
-        sys.argv = ['cl.py', 'stats', 'Afghanistan', '2020-01-01', '2020-01-12']
-        sys.stdout = StringIO()
-        cl.main()
-        printed_output = sys.stdout.getvalue()
-        self.assertEqual(printed_output, """Total cases in Afghanistan from 2020-01-01 to 2020-01-12: 0\n
-                         Total deaths in Afghanistan from 2020-01-01 to 2020-01-12: 0\n""")
         
     def test_cl_main_compare(self):
         """Test function for the command line main function when compare is requested"""
