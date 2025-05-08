ab12f61081bf7d5881d09f24ab2ab093b3a27b83
DanielZhang <danielzhang@Daniels-Laptop.local>
2025-05-07 19:35:09 -0500

    datasource

---
+++ ProductionCode/datasource.py
@@ -0,0 +1,39 @@
+import psycopg2
+import psql_Config as config
+
+class DataSource:
+
+    def __init__(self):
+        '''Constructor that initiates connection to database'''
+        self.connection = self.connect()
+
+    def connect(self):
+        '''Initiates connection to database using information in the psqlConfig.py file.
+        Returns the connection object.'''
+
+        try:
+            self.connection = psycopg2.connect(database=config.database, user=config.user, password=config.password, host="localhost")
+        except Exception as e:
+            print("Connection error: ", e)
+            exit()
+        return self.connection
+
+    def get_week_country_and_new_cases(self, country, date):
+        '''Returns the week, country and the number of new cases.'''
+        cursor = self.connection.cursor()
+        cursor.execute(f"SELECT {date}, {country}, New_cases FROM bigTable")
+        results = cursor.fetchall()
+        return results
+    
+    def get_specific(self):
+        '''Returns the values of Afghanistan in the table.'''
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM bigTable WHERE country =%s", ('Afghanistan',))
+        value = cursor.fetchall
+        return value
+    
+if __name__ == "__main__":
+    ds = DataSource()
+    results = ds.get_week_country_and_new_cases()
+    print(results)
+    ds.connection.close()
