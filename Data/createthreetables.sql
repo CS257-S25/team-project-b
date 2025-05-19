658f7a39e4b3f493e6628d02d8e8d33d77c77c44
Owen <heidtkeo@carleton.edu>
2025-05-19 15:00:12 -0400

    three tables done

--- Data/createthreetables.sql
+++ Data/createthreetables.sql
@@ -1,32 +1,28 @@
 -- Drop tables if they already exist (for repeated runs)
-DROP TABLE IF EXISTS covid_data, dates, countries, staging_raw_data;
 
-CREATE TABLE staging_raw_data (
-    report_date TEXT,
-    country_code TEXT,
-    country_name TEXT,
-    cases INT,
-    deaths INT
-);
+DROP TABLE IF EXISTS covid_data, dates, countries;
 
 CREATE TABLE countries (
-    country_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     country_code TEXT UNIQUE,
     country_name TEXT
 );
 
 CREATE TABLE dates (
-    date_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     report_date DATE UNIQUE
 );
 
 CREATE TABLE covid_data (
     data_id SERIAL PRIMARY KEY,
-    country_id INT REFERENCES countries(country_id),
-    date_id INT REFERENCES dates(date_id),
+    country_id int NOT NULL,
+    date_id int NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(id),
+    FOREIGN KEY (date_id) REFERENCES dates(id),
     cases INT,
     deaths INT
 );
+
 INSERT INTO countries (country_code, country_name)
 SELECT DISTINCT country_code, country_name
 FROM staging_raw_data
@@ -40,10 +36,10 @@ ON CONFLICT (report_date) DO NOTHING;
 
 INSERT INTO covid_data (country_id, date_id, cases, deaths)
 SELECT
-    c.country_id,
-    d.date_id,
+    c.id,
+    d.id,
     s.cases,
     s.deaths
 FROM staging_raw_data s
-JOIN countries c ON s.country_code = c.country_code
+JOIN countries c ON s.country_code = c.country_code 
 JOIN dates d ON TO_DATE(s.report_date, 'MM/DD/YY') = d.report_date;
