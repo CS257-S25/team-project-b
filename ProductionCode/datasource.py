47760fce873489695b8c9607fb36c4edf8139032
Fenan B Gudina <gudinaf@carleton.edu>
2025-05-27 19:15:23 -0500

    Update datasource.py

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -2,105 +2,121 @@ import psycopg2
 from . import psql_Config as config
 
 class DataSource:
+    """Manages database connection and queries for COVID-19 data."""
 
     def __init__(self):
-        '''Constructor that initiates connection to database'''
+        """Constructor that initiates connection to database."""
         self.connection = self.connect()
 
     def connect(self):
-        '''Initiates connection to database using information in the psqlConfig.py file.
-        Returns the connection object.'''
-
+        """Connect to the database using psql_Config."""
         try:
-            self.connection = psycopg2.connect(database=config.database, user=config.user, password=config.password, host="localhost")
+            return psycopg2.connect(
+                database=config.database,
+                user=config.user,
+                password=config.password,
+                host="localhost"
+            )
         except Exception as e:
-            print("Connection error: ", e)
+            print("Connection error:", e)
             exit()
-        return self.connection
-    
-    
+
     def get_sum_between_dates(self, country, start_date, end_date):
-        '''Returns the week, country and the number of new cases.'''
+        """Returns total cases and deaths for a country between two dates."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT SUM(New_cases), SUM(New_deaths) FROM bigTable WHERE Country = %s AND Date_reported BETWEEN %s AND %s", (country, start_date, end_date,))
+        cursor.execute("""
+            SELECT SUM(New_cases), SUM(New_deaths)
+            FROM bigTable
+            WHERE Country = %s AND Date_reported BETWEEN %s AND %s
+        """, (country, start_date, end_date))
         results = cursor.fetchone()
         cursor.close()
         return results
 
     def get_sum_specific(self, country, week):
-        '''Returns the week, country and the number of new cases.'''
+        """Returns total cases and deaths for a country on a specific date."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT SUM(New_cases), SUM(New_deaths) FROM bigTable WHERE Country = %s AND Date_reported = %s", (country, week,))
+        cursor.execute("""
+            SELECT SUM(New_cases), SUM(New_deaths)
+            FROM bigTable
+            WHERE Country = %s AND Date_reported = %s
+        """, (country, week))
         results = cursor.fetchone()
         cursor.close()
         return results
 
     def get_closest_date(self, target_date, country, before=True):
         """Get the closest available date for the country."""
-        try:
-            cursor = self.connection.cursor()
-            if before:
-                query = """
-                    SELECT MAX(Date_reported) FROM bigTable
-                    WHERE Country = %s AND Date_reported <= %s;
-                """
-            else:
-                query = """
-                    SELECT MIN(Date_reported) FROM bigTable
-                    WHERE Country = %s AND Date_reported >= %s;
-                """
-            cursor.execute(query, (country, target_date))
-            result = cursor.fetchone()
-            cursor.close()
-            return result[0] if result and result[0] else None
-        except Exception as e:
-            print("Error finding closest date:", e)
-            return None
-    
+        cursor = self.connection.cursor()
+        if before:
+            query = """
+                SELECT MAX(Date_reported) FROM bigTable
+                WHERE Country = %s AND Date_reported <= %s;
+            """
+        else:
+            query = """
+                SELECT MIN(Date_reported) FROM bigTable
+                WHERE Country = %s AND Date_reported >= %s;
+            """
+        cursor.execute(query, (country, target_date))
+        result = cursor.fetchone()
+        cursor.close()
+        return result[0] if result and result[0] else None
+
     def get_week_country_and_new_cases(self, country, date):
-        '''Returns the week, country and the number of new cases.'''
+        """Get new cases for a specific country on a specific date."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT New_cases FROM bigTable where Country =%s AND Date_reported =%s", (country, date,))
+        cursor.execute("""
+            SELECT New_cases FROM bigTable
+            WHERE Country = %s AND Date_reported = %s
+        """, (country, date))
         results = cursor.fetchall()
+        cursor.close()
         return results
-    
+
     def get_week_country_and_new_deaths(self, country, date):
-        '''Returns the week, country and the number of new cases.'''
+        """Get new deaths for a specific country on a specific date."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT New_deaths FROM bigTable where Country =%s AND Date_reported =%s", (country, date,))
+        cursor.execute("""
+            SELECT New_deaths FROM bigTable
+            WHERE Country = %s AND Date_reported = %s
+        """, (country, date))
         results = cursor.fetchall()
+        cursor.close()
         return results
-    
+
     def get_all_countries(self):
-        '''Returns a list of all country names from the bigTable.'''
+        """Returns a list of all country names from bigTable."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT DISTINCT Country FROM bigTable ORDER BY Country;")
+        cursor.execute("""
+            SELECT DISTINCT Country FROM bigTable ORDER BY Country;
+        """)
         countries = [row[0] for row in cursor.fetchall()]
         cursor.close()
         return countries
-    
+
     def get_stats(self, country, start_date, end_date):
+        """Returns daily cases and deaths for a country between two dates."""
         cursor = self.connection.cursor()
         cursor.execute("""
-            SELECT c.country_name, d.report_date, cd.cases, cd.deaths
-            FROM covid_data AS cd
-            JOIN countries AS c ON cd.country_id = c.id
-            JOIN dates AS d ON cd.date_id = d.id
-            WHERE c.country_name = %s AND d.report_date >= %s AND d.report_date <= %s
-            ORDER BY d.report_date
+            SELECT Country, Date_reported, New_cases, New_deaths
+            FROM bigTable
+            WHERE Country = %s AND Date_reported >= %s AND Date_reported <= %s
+            ORDER BY Date_reported
         """, (country, start_date, end_date))
         results = cursor.fetchall()
         cursor.close()
         return results
 
     def get_all_data(self):
-        '''Returns all rows from the bigTable.'''
+        """Returns all rows from bigTable as a list of dictionaries."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT Country, Date_reported, New_cases, New_deaths FROM bigTable")
+        cursor.execute("""
+            SELECT Country, Date_reported, New_cases, New_deaths FROM bigTable
+        """)
         rows = cursor.fetchall()
         cursor.close()
 
-        # Convert results to list of dictionaries for easier use
         data = []
         for row in rows:
             data.append({
@@ -113,10 +129,7 @@ class DataSource:
 
 if __name__ == "__main__":
     ds = DataSource()
-    results = ds.get_stats("Afghanistan", "2023-06-18", "20203-08-06")
-    cases_results = ds.get_week_country_and_new_cases("Afghanistan", "2023-08-06")
-    """results = ds.get_specific()"""
-    for i in results:
-        print(i)
-    print(cases_results[0][0])
+    results = ds.get_stats("Afghanistan", "2023-06-18", "2023-08-06")
+    for row in results:
+        print(row)
     ds.connection.close()
