e64f27f5b826601d7984e02c17b7061ade21fa89
Anthony Vazquez - Vazquez <vazqueza@carleton.edu>
2025-05-28 19:52:37 -0500

    errors

--- Tests/test_datasource.py
+++ Tests/test_datasource.py
@@ -95,8 +95,8 @@ class TestDataSource(unittest.TestCase):
 
     def test_get_all_countries(self):
         """Test get_all_countries returning a list of country names."""
-        self.mock_cursor.ds.get_all_countries.return_value = [
-            "Afghanistan", "Albania", "USA"
+        self.mock_cursor.fetchall.return_value = [
+            ("Afghanistan",), ("Albania",), ("USA",)
         ]
         result = self.ds.get_all_countries()
         self.assertEqual(result, ["Afghanistan", "Albania", "USA"])
